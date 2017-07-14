6a57bd7bfec43a3288646bffc26daf8546bd333f
Seren <SerenTera@users.noreply.github.com>
2017-07-14 14:18:36 +0800

    Changed messaging defaults

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ To discover more ids, hook S_SPAWN_NPC and check huntingzoneid and templateId.
 //Defaults:
 let	enabled=true, //default enabling of module(default true)
 	markenabled=true,  //default enabling of markers(default true)
-	messager=true,  //default enabling of system chat message (default false)
+	messager=false,  //default enabling of system chat message (default false)
 	alerted=true;	//default enabling of system notice (default true)
 	
 //Monster ids and other values:
@@ -20,25 +20,25 @@ const mobzone = [1023], //huntingzoneid of mob
 	
 //------------------------------------All defaults and changeable values are above this line------------------------------------------------------------------//
 
-const Command = require('command');
+const Command = require('command')
 
 module.exports = function markmob(dispatch) {
-	const command = Command(dispatch);
+	const command = Command(dispatch)
 
 	let	mobid=[];
 		
 	command.add('warntoggle',() => {
 		if(enabled) {
-			for(let items of mobid) {
-				despawnthis(items)
-			};
+			for(let itemid of mobid) {
+				despawnthis(itemid)
+			}
 			enabled=false,
 			command.message('(Warnme) Module Disabled')
 		}
 		else
 			enabled=true,
 			command.message('(Warnme) Module Enabled')
-	});
+	})
 	
 	command.add('warnalert',() => {
 		if(alerted) {
@@ -48,7 +48,7 @@ module.exports = function markmob(dispatch) {
 		else
 			alerted=true,
 			command.message('(Warnme)System popup notice enabled')
-	});
+	})
 	
 	command.add('warnmarker',() => {
 		if(markenabled) {
@@ -58,40 +58,40 @@ module.exports = function markmob(dispatch) {
 		else
 			markenabled=true,
 			command.message('(Warnme)Item Markers enabled')
-	});
+	})
 	
 	command.add('warnclear',() => {
 		for(let itemid of mobid) {
 			despawnthis(itemid)
-		};
+		}
 		command.message('(Warnme)Item Markers Clear Attempted')
-	});
+	})
 	
 	dispatch.hook('S_SPAWN_NPC', 3, event => {
 		if(enabled && mobzone.includes(event.huntingZoneId) && mobtemplate.includes(event.templateId)) { 
 			if(markenabled) {
 				markthis(event.x,event.y,event.z,event.id.low), //uint64 id makes .includes() method unable to work? >.>
 				mobid.push(event.id.low)
-			};
+			}
 			if(alerted) {
 				notice('Found '+custommsg)
-			};
+			}
 			if(messager) {
 				command.message('(Warnme)Found '+custommsg)
-			};
-		};
-	}); 
+			}
+		}
+	}) 
 
 	dispatch.hook('S_DESPAWN_NPC', 1, event => {
 		if(mobid.includes(event.target.low)) {
 			despawnthis(event.target.low),
 			mobid.splice(mobid.indexOf(event.target.low),1)
-		};
-	}); 
+		}
+	})
 	
 	dispatch.hook('S_LOAD_TOPO',1, event => { //reset mobid list on location change
 		mobid=[]
-	});
+	})
 	
 	function markthis(locationx,locationy,locationz,idRef) {
 		dispatch.toClient('S_SPAWN_DROPITEM', 1, {
@@ -103,14 +103,14 @@ module.exports = function markmob(dispatch) {
 			amount: 1,
 			expiry: 300000, //expiry time,milseconds (300000=5 mins?)
 			owners: [{id: 0}]
-		});	
-	};
+		})
+	}
 	
 	function despawnthis(despawnid) {
 		dispatch.toClient('S_DESPAWN_DROPITEM', 1, {
 			id: {low:despawnid,high:0,unsigned:true}
-		});
-	};
+		})
+	}
 	
 	function notice(msg) {
 		dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
@@ -118,6 +118,6 @@ module.exports = function markmob(dispatch) {
             unk2: 0,
             unk3: 0,
             message: '(Proxy)' + msg
-        });
-    };
-};
+        })
+    }
+}
