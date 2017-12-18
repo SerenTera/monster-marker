408286a366001c30c4d8fed0cfa361041b1e2ce8
Seren <cmingxuan94@gmail.com>
2017-12-18 22:59:49 +0800

    Disable Blue box

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const Item_ID = 98260,			//ItemId for the marker, Use different itemids if you f
 		'51_7011'		: 'Linyphi',
 		'53_1000'		: 'Divine Reaver',
 		'52_9050'		: 'Yunaras Snaggletooth',
-		'1023_8888888'	: 'Blue Box',
+		//'1023_8888888'	: 'Blue Box',
 		//'181_2023'		: 'Test mob',
 		
 	  
